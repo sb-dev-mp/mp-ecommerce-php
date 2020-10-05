f5f2cb16098d47c7d3053d46d7da9f6f7ab11c26
Stefano Boccuti <s.boccuti+mp@gmail.com>
2020-10-05 07:29:52 -0300

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,6 +1,6 @@
 <?php
   if (isset($_POST)) {
-    file_put_contents(__DIR__ . '/log.json', json_encode($_POST));
+    file_put_contents(__DIR__ . '/log.json', json_encode($_GET));
     http_response_code(200);
   } else  {
   }
