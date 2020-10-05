a0f93c099b8b44b3c6535c6329a7875318e515f4
Stefano Boccuti <s.boccuti+mp@gmail.com>
2020-10-05 08:17:08 -0300

    Update notification.php

--- notification.php
+++ notification.php
@@ -1,6 +1,9 @@
 <?php
+  $entityBody = file_get_contents('php://input');
   if (isset($_POST)) {
-    file_put_contents(__DIR__ . '/log.json', json_encode($_GET));
+    if ($entityBody != NULL && $entityBody->action == "payment.created")
+      file_put_contents(__DIR__ . '/log.json', json_encode($entityBody));
     http_response_code(200);
   } else  {
+    file_put_contents(__DIR__ . '/log.txt', json_encode($entityBody));
   }
